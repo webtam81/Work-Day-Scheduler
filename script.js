71ed4ca30eed4056b18af21f9ed6824c5b19a29c
webtam81 <t.h@tammyharris.co.uk>
2023-09-12 16:54:24 +0100

    created time blocks without styling

--- script.js
+++ script.js
@@ -14,27 +14,42 @@ dayHeadingEl.text(dayjs().format("dddd, DD MMMM YYYY")); //set date
 
 
 //time blocks for each hour (9am - 5pm)
-let blockTime = dayStartTime;
+let blockTime = dayStartTime; //time of this time block
 for (h = 0; h < totalHours + 1; h++) {
 
-    //create div
+    //bootstrap row
+    let newRowEl = $('<div>')
+        .addClass('row');
+
+    //column for time
     let timeDivEl = $('<div>')
-        .addClass('col')
-        .text(blockTime.format("h A"))
-        //.$('</div>');
+        .addClass('col time')
+        .text(blockTime.format("h A"));
+        
+    //column for task
+    let taskDivEl = $('<div>')
+    .addClass('col task');
+
+    //column for button
+    let btnDivEl = $('<div>')
+    .addClass('col save');
     
     //console.log(blockTime); //todo RM
     //console.log(blockTime.format("h A")); //todo RM
 
-    timeBlocksEl.append(timeDivEl);
+    timeBlocksEl.append(newRowEl);
+    newRowEl.append(timeDivEl);
+    newRowEl.append(taskDivEl);
+    newRowEl.append(btnDivEl);
 
-    blockTime = blockTime.add(1, 'h');
+    blockTime = blockTime.add(1, 'h'); //add 1 hour
+
 }
  
 //TODO
 //colour code time blocks 
 //enter event in block when clicked
-//save event in locat storage
+//save event in local storage
 
 //DONE
 //current day at top of calendar
