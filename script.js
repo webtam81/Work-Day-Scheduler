b1be74099393fd5f4ebdb8c9114234ea798d15e4
webtam81 <t.h@tammyharris.co.uk>
2023-09-12 14:00:13 +0100

    added date to top of page

---
+++ script.js
@@ -0,0 +1,14 @@
+//VARIABLES
+let dayHeading = $('#currentDay');
+dayHeading.text(dayjs().format("dddd, DD MMMM YYYY"));
+
+
+//TODO
+//time blocks for each hour (9am - 5pm)
+//colour code time blocks 
+//enter event in block when clicked
+//save event in locat storage
+
+//DONE
+//current day at top of calendar
+
